2a172e962ed1eb2867f3edcd4a497bc4e89ad617
Praveen-Bharath <91447499+Praveen-Bharath@users.noreply.github.com>
2021-11-19 10:38:21 +0530

    Update main-script.js

--- main-script.js
+++ main-script.js
@@ -1,59 +1,70 @@
 var ItemArray = [
   {
+    id:1,
     name: "Biriyani",
     img: "./Images/briyani.jpg",
     type: "lunch",
   },
   {
+    id:2,
     name: "Chappathi",
     img: "./Images/chapathi.jpg",
     type: "breakfast",
   },
   {
+    id:3,
     name: "Curd",
     img: "./Images/curd.jpg",
     type: "lunch",
   },
   {
+    id:4,
     name: "Idly",
     img: "./Images/idly.jpg",
     type: "breakfast",
   },
   {
+    id:5,
     name: "Lemon",
     img: "./Images/lemon.jpg",
     type: "lunch",
   },
   {
+    id:6,
     name: "Meals",
     img: "./Images/meals.jpg",
     type: "lunch",
   },
   {
+    id:7,
     name: "Naan",
     img: "./Images/naan.jpg",
     type: "dinner",
   },
   {
+    id:8,
     name: "Noodles",
     img: "./Images/noodles.jpg",
     type: "dinner",
   },
   {
+    id:9,
     name: "Pongal",
     img: "./Images/pongal.jpg",
     type: "breakfast",
   },
   {
+    id:10,
     name: "Poori",
     img: "./Images/poori.jpg",
     type: "breakfast",
   },
   {
+    id:11,
     name: "Roast",
     img: "./Images/roast.jpg",
     type: "dinner",
-  },
+  }
 ];
 var div = document.getElementById("itemlist");
 function renderItems(itemName) {
